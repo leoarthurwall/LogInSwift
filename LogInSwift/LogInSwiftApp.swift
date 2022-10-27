309781e53f58bb04099587990d1c1b64dbfc79c4
leoarthurwall <leowall93@gmail.com>
2022-10-27 18:11:56 +0200

    Initial Commit

---
+++ LogInSwift/LogInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInSwiftApp.swift
+//  LogInSwift
+//
+//  Created by Leo Wall on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LogInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
