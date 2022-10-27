46981bd18401498b0959c74495a5977319ae976b
leoarthurwall <leowall93@gmail.com>
2022-10-27 18:20:11 +0200

    added the background colour to blue and placed in a z stack

--- LogInSwift/ContentView.swift
+++ LogInSwift/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Login Page!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea()
+                Text("Hello, Login Page!")
+                    .padding()
+            }
+        }
     }
 }
 
